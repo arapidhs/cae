a3b8a472b865aa34a187d217a4c25c6b20786eaf
arapidhs <arapidhs@gmail.com>
2025-05-20 23:48:01 +0300

    Add F1 control.

--- src/main/java/com/dungeoncode/ca/view/Controls.java
+++ src/main/java/com/dungeoncode/ca/view/Controls.java
@@ -40,6 +40,7 @@ public class Controls {
         controls.add(new Control("-", "Dec. resolution"));
         controls.add(new Control("→", "Next palette"));
         controls.add(new Control("←", "Previous palette"));
+        controls.add(new Control("F1", "Invert palette"));
         controls.add(new Control("Ctrl+s", "Save screen as image"));
 
         mouseControls = new ArrayList<>();
